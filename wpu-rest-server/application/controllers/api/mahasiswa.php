27bfe58fa87ee7f7219e3c8fb69754b77f13930f
auraafa <aurafadhlin@gmail.com>
2025-05-31 23:16:27 +0700

    perbaruan wpu-rest-server

--- wpu-rest-server/application/controllers/api/mahasiswa.php
+++ wpu-rest-server/application/controllers/api/mahasiswa.php
@@ -3,17 +3,18 @@
 use RestServer\Libraries\REST_Controller;
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-class mahasiswa extends REST_Controller {
+/**
+ * @property Mahasiswa_model $mahasiswa
+ */
+class Mahasiswa extends REST_Controller {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('Mahasiswa_model', 'mahasiswa');
-
-        $this->methods['index_get']['limit'] = 10000;
+        $this->methods['index_get']['limit'] = 100000;
     }
 
     public function index_get() {
@@ -26,25 +27,24 @@ class mahasiswa extends REST_Controller {
 
         if ($mahasiswa) {
             $this->response([
-                    'status' => true,
-                    'data' => $mahasiswa
-                ], REST_Controller::HTTP_OK);
+                'status' => true,
+                'data' => $mahasiswa
+            ], REST_Controller::HTTP_OK);
         } else {
             $this->response([
-                    'status' => false,
-                    'message' => 'id not found'
-                ], REST_Controller::HTTP_NOT_FOUND);
+                'status' => false,
+                'message' => 'id not found'
+            ], REST_Controller::HTTP_NOT_FOUND);
         }
     }
 
     public function index_delete(){
         $id = $this->delete('id');
-
         if ($id == null) {
             $this->response([
-                    'status' => false,
-                    'message' => 'provide an id'
-                ], REST_Controller::HTTP_BAD_REQUEST);
+                'status' => false,
+                'message' => 'provide an id'
+            ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
             if ($this->mahasiswa->deleteMahasiswa($id) > 0) {
                 $this->response([
@@ -52,7 +52,6 @@ class mahasiswa extends REST_Controller {
                     'id' => $id,
                     'message' => 'deleted.'
                 ], REST_Controller::HTTP_NO_CONTENT);
-
             } else {
                 $this->response([
                     'status' => false,
@@ -70,25 +69,20 @@ class mahasiswa extends REST_Controller {
             'jurusan' => $this->post('jurusan')  
         ];
 
-
         if ($this->mahasiswa->createMahasiswa($data) > 0) {
             $this->response([
-                    'status' => true,
-                    'message' => 'new mahasiswa has been created.'
-                ], REST_Controller::HTTP_CREATED);
+                'status' => true,
+                'message' => 'new mahasiswa has been created.'
+            ], REST_Controller::HTTP_CREATED);
         } else {
             $this->response([
-                    'status' => false,
-                    'message' => 'failed to create new data'
-                ], REST_Controller::HTTP_BAD_REQUEST);
+                'status' => false,
+                'message' => 'failed to create new data'
+            ], REST_Controller::HTTP_BAD_REQUEST);
         }
-
     }
 
-
-
-     public function index_put(){
-
+    public function index_put(){
         $id = $this->put('id');
         $data = [
             'nrp' => $this->put('nrp'),
@@ -97,19 +91,17 @@ class mahasiswa extends REST_Controller {
             'jurusan' => $this->put('jurusan')  
         ];
 
-
         if ($this->mahasiswa->updateMahasiswa($data, $id) > 0) {
             $this->response([
-                    'status' => true,
-                    'message' => 'data mahasiswa has been updated.'
-                ], REST_Controller::HTTP_CREATED);
+                'status' => true,
+                'message' => 'data mahasiswa has been updated.'
+            ], REST_Controller::HTTP_CREATED);
         } else {
             $this->response([
-                    'status' => false,
-                    'message' => 'failed to update data'
-                ], REST_Controller::HTTP_BAD_REQUEST);
+                'status' => false,
+                'message' => 'failed to update data'
+            ], REST_Controller::HTTP_BAD_REQUEST);
         }
     }
-
 
 }
